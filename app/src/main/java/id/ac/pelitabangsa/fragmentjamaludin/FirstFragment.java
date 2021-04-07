e7c4c4c2eef4e5a4c38295a75eacecc5b8c61ca0
JAMALUDIN32 <jamaludin3234@gmail.com>
2021-04-07 22:03:53 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/pelitabangsa/fragmentjamaludin/FirstFragment.java
@@ -0,0 +1,6 @@
+package id.ac.pelitabangsa.fragmentjamaludin;
+
+import android.app.Fragment;
+
+public class FirstFragment extends Fragment {
+}
---
+++ app/src/main/java/id/ac/pelitabangsa/fragmentjamaludin/SekondActivity.java
@@ -0,0 +1,6 @@
+package id.ac.pelitabangsa.fragmentjamaludin;
+
+import android.app.Activity;
+
+public class SekondActivity extends Activity {
+}
